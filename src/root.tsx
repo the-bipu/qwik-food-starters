fb85b6d0c9ed31d32d3e08513c38ac1cbb55e468
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-09-24 23:53:54 +0530

    learned routing
    and also the states in qwik, also learned about the rendering of contents in browser and how qwik is better than others

--- src/root.tsx
+++ src/root.tsx
@@ -29,7 +29,7 @@ export default component$(() => {
         )}
         <RouterHead />
       </head>
-      <body lang="en">
+      <body lang="en" class='bg-slate-200'>
         <RouterOutlet />
         {!isDev && <ServiceWorkerRegister />}
       </body>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,18 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const user = useSignal("You (yes, i'm talking to you)");
+
   return (
     <>
-      <h1>Hi 👋</h1>
+      <h1>Hi, {user.value} 👋</h1>
       <div>
         Can't wait to see what you build with qwik!
         <br />
         Happy coding.
       </div>
+      <button class='bg-black text-white p-2 rounded' onClick$={() => user.value = 'Don"t afraid. It"s just some dark magic... 😆'}>Click to change the name...</button>
     </>
   );
 });
